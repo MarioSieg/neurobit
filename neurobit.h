760d080a078455747c67950585a34fd908c7422a
Mario Sieg <mario.sieg.64@gmail.com>
2025-06-20 15:16:06 +0000

    Add tests for uint4

--- neurobit.h
+++ neurobit.h
@@ -55,20 +55,20 @@ extern void neurobit_dequant_uint2_to_float(const uint8_t* in, float* out, size_
 #include <math.h>
 #include <assert.h>
 
-static inline uint8_t saturate_u4(int32_t x) { return x < 0 ? 0 : x > 0xF ? 0xF : x; }
-static inline uint8_t saturate_u2(int32_t x) { return x < 0 ? 0 : x > 3 ? 3 : x; }
+static inline uint8_t sat_u4(int32_t x) { return x < 0 ? 0 : x > 15 ? 15 : x; }
+static inline uint8_t sat_u2(int32_t x) { return x < 0 ? 0 : x > 3 ? 3 : x; }
 
 void neurobit_quant_float_to_uint4(const float* in, uint8_t* out, size_t len, float scale, int32_t zero_point) {
     assert(in && out && scale > 0.f);
     float inv_scale = 1.0f / scale;
     size_t i;
     for (i=0; i+1 < len; i += 2) {
-        uint8_t a = saturate_u4(zero_point + (int32_t)roundf(in[i]*inv_scale));
-        uint8_t b = saturate_u4(zero_point + (int32_t)roundf(in[i+1]*inv_scale));
+        uint8_t a = sat_u4(zero_point + (int32_t)roundf(in[i]*inv_scale));
+        uint8_t b = sat_u4(zero_point + (int32_t)roundf(in[i+1]*inv_scale));
         out[i>>1] = a | b<<4;
     }
     if (len & 1) /* Odd tail */
-        out[i>>1] = saturate_u4(zero_point + (int32_t)roundf(in[len-1]*inv_scale));
+        out[i>>1] = sat_u4(zero_point + (int32_t)roundf(in[len-1]*inv_scale));
 }
 
 void neurobit_dequant_uint4_to_float(const uint8_t* in, float* out, size_t len, float scale, int32_t zero_point) {
@@ -76,12 +76,12 @@ void neurobit_dequant_uint4_to_float(const uint8_t* in, float* out, size_t len, 
     size_t i, j;
     for (i=0, j=0; i+1 < len; i += 2, ++j) {
         uint8_t p = in[j];
-        uint8_t a = p & 0xF, b = p >> 4;
+        uint8_t a = p & 15, b = p >> 4;
         out[i] = (float)((int32_t)a - zero_point)*scale;
         out[i+1] = (float)((int32_t)b - zero_point)*scale;
     }
     if (len & 1) /* Odd tail */
-        out[len-1] = (float)((in[i>>1] & 0xF) - zero_point)*scale;
+        out[len-1] = (float)((in[i>>1] & 15) - zero_point)*scale;
 }
 
 void neurobit_quant_float_to_uint2(const float* in, uint8_t* out, size_t len, float scale, int32_t zero_point) {
@@ -89,19 +89,19 @@ void neurobit_quant_float_to_uint2(const float* in, uint8_t* out, size_t len, fl
     float inv_scale = 1.0f / scale;
     size_t i;
     for (i=0; i+3 < len; i += 4) {
-        uint8_t a = saturate_u2(zero_point + (int32_t)roundf(in[i]*inv_scale));
-        uint8_t b = saturate_u2(zero_point + (int32_t)roundf(in[i+1]*inv_scale));
-        uint8_t c = saturate_u2(zero_point + (int32_t)roundf(in[i+2]*inv_scale));
-        uint8_t d = saturate_u2(zero_point + (int32_t)roundf(in[i+3]*inv_scale));
+        uint8_t a = sat_u2(zero_point + (int32_t)roundf(in[i]*inv_scale));
+        uint8_t b = sat_u2(zero_point + (int32_t)roundf(in[i+1]*inv_scale));
+        uint8_t c = sat_u2(zero_point + (int32_t)roundf(in[i+2]*inv_scale));
+        uint8_t d = sat_u2(zero_point + (int32_t)roundf(in[i+3]*inv_scale));
         out[i>>2] = (uint8_t)(a | (b<<2) | (c<<4) | (d<<6));
     }
 
     if (len & 3) { /* Handle 1-, 2- or 3-value tail */
         uint8_t p = 0;
         switch (len & 3) {
-            case 3: p |= saturate_u2(zero_point + (int32_t)roundf(in[i+2]*inv_scale)) << 4;
-            case 2: p |= saturate_u2(zero_point + (int32_t)roundf(in[i+1]*inv_scale)) << 2;
-            case 1: p |= saturate_u2(zero_point + (int32_t)roundf(in[i]*inv_scale));
+            case 3: p |= sat_u2(zero_point + (int32_t)roundf(in[i+2]*inv_scale)) << 4;
+            case 2: p |= sat_u2(zero_point + (int32_t)roundf(in[i+1]*inv_scale)) << 2;
+            case 1: p |= sat_u2(zero_point + (int32_t)roundf(in[i]*inv_scale));
         }
         out[i>>2] = p;
     }
@@ -112,10 +112,10 @@ void neurobit_dequant_uint2_to_float(const uint8_t* in, float* out, size_t len, 
     size_t i, j;
     for (i=0, j=0; i+3 < len; i += 4, ++j) {
         uint8_t p = in[j];
-        out[i] = (float)((p&3) - zero_point)*scale;
-        out[i+1] = (float)((p>>2&3) - zero_point)*scale;
-        out[i+2] = (float)((p>>4&3) - zero_point)*scale;
-        out[i+3] = (float)((p>>6&3) - zero_point)*scale;
+        out[i] = (float)((p & 3) - zero_point)*scale;
+        out[i+1] = (float)((p>>2 & 3) - zero_point)*scale;
+        out[i+2] = (float)((p>>4 & 3) - zero_point)*scale;
+        out[i+3] = (float)((p>>6 & 3) - zero_point)*scale;
     }
     if (len & 3) { /* Handle 1-, 2- or 3-value tail */
         uint8_t p = in[i>>2];
--- test.c
+++ test.c
@@ -7,6 +7,58 @@
 #define NEUROBIT_IMPLEMENTATION
 #include "neurobit.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+static void test_uint4_quant_roundtrip(void) {
+    printf("Testing uint4... quantization\n");
+    const float scale = 0.149460852f;
+    const int32_t zp = 8;
+    const float input[7] = {
+        0.851733685f,
+        -0.876200974f,
+        -0.250579119f,
+        0.802029967f,
+        0.721749306f,
+        -0.72338897f,
+        -0.959288836f,
+    };
+    float dequantized_correct[7] = {
+        1.7484988f,
+        -1.77296615f,
+        -0.549500823f,
+        1.54933429f,
+        1.46905351f,
+        -1.47069323f,
+        -1.85605395f,
+    };
+    for (int i=0; i < 7; ++i) {
+        dequantized_correct[i] -= input[i]; /* Because I got the test data from piquant with reduction op add, accidentally to we subtract the input to get the original values */
+    }
+    const uint8_t correct_quantized[4] = {46, 214, 61, 2};
+
+    uint8_t quantized[4] = {};
+    neurobit_quant_float_to_uint4(input, quantized, sizeof(input)/sizeof(*input), scale, zp);
+    for (int i=0; i < 4; ++i) {
+        if (quantized[i] != correct_quantized[i]) {
+            printf("Quantized bits at index %d are not correct\n", i);
+            abort();
+        }
+    }
+
+    float dequantized[7] = {};
+    neurobit_dequant_uint4_to_float(quantized, dequantized, sizeof(dequantized)/sizeof(*dequantized), scale, zp);
+    for (int i=0; i < 7; ++i) {
+        if (fabsf(dequantized[i] - dequantized_correct[i]) > 1e-7) {
+            printf("Dequantized floats at index %d are not correct\n", i);
+            abort();
+        }
+    }
+
+    printf("Test passed\n");
+}
+
 int main(void) {
+    test_uint4_quant_roundtrip();
     return 0;
 }
